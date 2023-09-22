33c182209bf91bbd10ca5e9a65a572c476988e15
jlnikhilrao <nikhilraojl@outlook.com>
2023-09-22 19:17:08 +0530

    Add powershell autocomplete for 'op' command

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,2 +1,15 @@
 
 $PSStyle.FileInfo.Directory = ''
+
+$PROJECTSPATH = "$HOME\Projects\"
+$IGNOREDIR = "$HOME\Projects\deploys*"
+$opCommandCompletion = {
+    param($stringMatch)
+    $items  = @(Get-ChildItem -Path "$PROJECTSPATH\*\$stringMatch*" -Directory | 
+        Where-Object {$_.fullname -notlike $IGNOREDIR } |
+        Select-Object -ExpandProperty name )
+    
+    $items
+}
+
+Register-ArgumentCompleter -Native -CommandName op -ScriptBlock $opCommandCompletion
