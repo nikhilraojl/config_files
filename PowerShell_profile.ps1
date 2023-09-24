61bfb50b1386925ca985b7562ee054a490c5b7be
jlnikhilrao <nikhilraojl@outlook.com>
2023-09-24 18:32:47 +0530

    Remap prediction key bindings

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -12,4 +12,19 @@ $opCommandCompletion = {
     $items
 }
 
+Set-PSReadLineKeyHandler -Key Ctrl+l `
+                         -BriefDescription RemapForwardToCtrlL `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
+}
+
+Set-PSReadLineKeyHandler -Key Ctrl+Shift+l `
+                         -BriefDescription RemapAceeptNextCharToCtrlL `
+                         -ScriptBlock {
+    param($key, $arg)
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
+}
+
 Register-ArgumentCompleter -Native -CommandName op -ScriptBlock $opCommandCompletion
