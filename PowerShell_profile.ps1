e305a96a9201e179ceb71d6a9aceee1681ff7b3c
jlnikhilrao <nikhilraojl@outlook.com>
2024-01-07 00:31:37 +0530

    Update pwsh 'op' autocomplete

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -5,28 +5,45 @@ $PSStyle.FileInfo.Directory = ''
 $PROJECTSPATH = "$HOME\Projects\"
 $IGNOREDIR = "$HOME\Projects\deploys*"
 $opCommandCompletion = {
-    param($stringMatch)
-    $items  = @(Get-ChildItem -Path "$PROJECTSPATH\*\$stringMatch*" -Directory | 
-        Where-Object {$_.fullname -notlike $IGNOREDIR } |
-        Select-Object -ExpandProperty name )
+    # For why should we use these 3 specific parameters 
+    # see powershell docs for `register-argumentcompleter` for `Native` command
+    param ( $wordToComplete,
+        $commandAst,
+        $cursorPosition )
     
-    $items
+    # The next couple of `if` blocks are there to stop autocomplete
+    # after the first argument. Maybe a better way exists but this is
+    # works for now
+    $spaces = $commandAst.ToString().Split(" ").GetUpperBound(0)
+    if (($wordToComplete -and ($spaces -gt 1)) ) {
+        return
+    }
+    if ((-not $wordToComplete -and ($spaces -ge 1)) ) {
+        return
+    }
+
+    # autocomplete list
+    $items = @(Get-ChildItem -Path "$PROJECTSPATH\*\$wordToComplete*" -Directory | 
+        Where-Object { $_.fullname -notlike $IGNOREDIR } |
+        Select-Object -ExpandProperty name )
+
+    return $items
 }
 Register-ArgumentCompleter -Native -CommandName op -ScriptBlock $opCommandCompletion
 
 # Full autocomplete predictive text
 Set-PSReadLineKeyHandler -Key Ctrl+l `
-                         -BriefDescription RemapForwardToCtrlL `
-                         -ScriptBlock {
+    -BriefDescription RemapForwardToCtrlL `
+    -ScriptBlock {
     param($key, $arg)
 
     [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar($key, $arg)
 }
 
-# Full autocomplete predictive text
+# Single word autocomplete predictive text
 Set-PSReadLineKeyHandler -Key Ctrl+Shift+l `
-                         -BriefDescription RemapAceeptNextCharToCtrlL `
-                         -ScriptBlock {
+    -BriefDescription RemapAceeptNextCharToCtrlL `
+    -ScriptBlock {
     param($key, $arg)
     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptNextSuggestionWord($key, $arg)
 }
