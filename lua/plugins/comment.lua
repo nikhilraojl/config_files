ca1278e34b9a3e89ae05d23311f5a4c7581c965c
nikhilraojl <nikhilraojl@outlook.com>
2025-12-07 11:29:06 +0530

    Fix failing keymap for comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -6,7 +6,12 @@ return {
         local comment_api = require("Comment.api")
         -- use ctrl+/ (which should be represented as [<C-_>] for some reason)
         -- for commenting single lines and multiple selected lines
-        vim.keymap.set("n", "<C-_>", comment_api.toggle.linewise.current)
-        vim.keymap.set("x", "<C-_>", comment_api.call("toggle.linewise", "g@"), { expr = true })
+        if vim.loop.os_uname().sysname == "Linux" then
+            vim.keymap.set("n", "<C-/>", comment_api.toggle.linewise.current)
+            vim.keymap.set("x", "<C-/>", comment_api.call("toggle.linewise", "g@"), { expr = true })
+        else
+            vim.keymap.set("n", "<C-_>", comment_api.toggle.linewise.current)
+            vim.keymap.set("x", "<C-_>", comment_api.call("toggle.linewise", "g@"), { expr = true })
+        end
     end,
 }
